898543a5b4598a66c1a66099d68cc104a6e89f7d
Emilse Arias <emilsearias@gmail.com>
2024-07-08 11:21:02 -0300

    dibujamos el numero de filas que pide el usuario

--- js/ejercicios/tp2-ejercicio10.js
+++ js/ejercicios/tp2-ejercicio10.js
@@ -4,12 +4,12 @@ const filas = parseInt(prompt('Ingrese un número de filas'))
 const columnas = parseInt(prompt('Ingrese un número de columnas'))
 
 document.write(`<table><tbody>`)
-document.write(`<tr>`)
-
-for(let indiceColumnas = 1; indiceColumnas <= columnas; indiceColumnas++){
-    document.write(`<td>1</td>`)
+for(let indiceFilas = 1; indiceFilas <= filas; indiceFilas++){
+    document.write(`<tr>`)
+    for(let indiceColumnas = 1; indiceColumnas <= columnas; indiceColumnas++){
+        document.write(`<td>1</td>`)
+    }
+    document.write(`</tr>`)
 }
-
-document.write(`</tr>`)
 document.write(`</tbody></table>`)      
     
