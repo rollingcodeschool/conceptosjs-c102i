35f3fdc154d0a2caa4162cf13514229f35244000
Emilse Arias <emilsearias@gmail.com>
2024-07-10 11:33:56 -0300

    agregamos un ejemplo de funcion declarativa sin parametros para saludar

---
+++ js/06-funciones.js
@@ -0,0 +1,14 @@
+//funciones declarativas
+//funcion sin parametros
+
+function saludar(){
+    //todo el codigo que quiero agregar para saludar
+    document.write('<p>Hola a mundo 🖐</p>')
+}
+
+//invocar o llamar a una funcion
+saludar();
+
+document.write('<p>tareas varias</p>');
+
+saludar();
