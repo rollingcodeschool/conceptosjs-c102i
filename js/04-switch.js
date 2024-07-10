5369955cc0c21bc904098a872e68feafd0da635d
Emilse Arias <emilsearias@gmail.com>
2024-07-10 10:06:06 -0300

    agregamos cambios en la estructura switch

--- js/04-switch.js
+++ js/04-switch.js
@@ -1,7 +1,4 @@
 // numero 1 - domingo hasta el 7 = sabado
-
-const dia = parseInt(prompt('Ingresa el numero del día'));
-
 // if(dia === 1){
 //     document.write('Hoy es domingo')
 // }else if(dia === 2){
@@ -12,18 +9,26 @@ const dia = parseInt(prompt('Ingresa el numero del día'));
 //     document.write('Hoy es miercoles')
 // }
 
-switch(dia){
-    case 1:
-        document.write('Hoy es domingo');
-        break;
-    case 2:
-        document.write('Hoy es lunes');
-        break;
-    case 3:
-        document.write('Hoy es martes');
-        break;
+do {
+  let dia = prompt("Ingresa el numero del día: 1- Domingo, 2- Lunes, 3-Martes");
+  console.log(new Date());
+  dia = dia.toLowerCase(); //aqui transformo a minuscula
+  // dia = dia.toUpperCase(); //aqui transformo a mayuscula
+
+  switch (dia) {
+    case "1":
+    case "domingo":
+      document.write("Hoy es domingo");
+      console.log("Hoy es domingo")
+      break;
+    case "2":
+      document.write("Hoy es lunes");
+      console.log("Hoy es lunes")
+    case "3":
+      document.write("Hoy es martes");
+      console.log("Hoy es martes")
+      break;
     default:
-        document.write('Ingresaste un valor invalido');
-}
-
-
+      document.write("Ingresaste un valor invalido");
+  }
+} while (confirm("¿Quieres seleccionar otra opción?"));
