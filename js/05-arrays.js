40a27395b528e7571f4b668f539545972adc123e
Emilse Arias <emilsearias@gmail.com>
2024-07-15 11:07:52 -0300

    mejoramos el codigo de arrays agregando una funcion que muestra las lista de las comidas

--- js/05-arrays.js
+++ js/05-arrays.js
@@ -13,6 +13,17 @@ const comidas = [
   "Empanadas",
 ];
 
+//aqui agrego las funciones
+const mostrarComidas = ()=>{
+    document.write(`<h3>Lista de comidas, elementos: ${comidas.length}</h3>`);
+    document.write(`<ul>`);
+    for(let i = 0; i < comidas.length; i++){
+        document.write(`<li>${comidas[i]}</li>`);
+    }
+    document.write(`</ul>`);
+}
+
+
 document.write(`<p>El array de comidas tiene ${comidas.length} elementos</p>`);
 document.write(`<p>La primer comida es: ${comidas[0]} </p>`);
 document.write(`<p>La segunda comida es: ${comidas[1]} </p>`);
@@ -20,95 +31,43 @@ document.write(`<p>Las comidas son: ${comidas} </p>`);
 console.log(comidas);
 
 //mostrar los elementos del array como una lista
-document.write(`<h3>Lista de comidas, elementos: ${comidas.length}</h3>`);
-document.write(`<ul>`);
-for(let i = 0; i < comidas.length; i++){
-    document.write(`<li>${comidas[i]}</li>`);
-}
-document.write(`</ul>`);
+mostrarComidas();
 
 // modificar elementos en un array
 comidas[2] = 'locro';
-
-document.write(`<h3>Lista de comidas, elementos: ${comidas.length}</h3>`);
-document.write(`<ul>`);
-for(let i = 0; i < comidas.length; i++){
-    document.write(`<li>${comidas[i]}</li>`);
-}
-document.write(`</ul>`);
+mostrarComidas();
 
 // agregar elementos en un array
 // al principio
 comidas.unshift('tira de asado','guiso')
-
-document.write(`<h3>Lista de comidas, elementos: ${comidas.length}</h3>`);
-document.write(`<ul>`);
-for(let i = 0; i < comidas.length; i++){
-    document.write(`<li>${comidas[i]}</li>`);
-}
-document.write(`</ul>`);
+mostrarComidas();
 
 //agregar elementos al final
 comidas.push('ravioles', 'sorrentinos')
-document.write(`<h3>Lista de comidas, elementos: ${comidas.length}</h3>`);
-document.write(`<ul>`);
-for(let i = 0; i < comidas.length; i++){
-    document.write(`<li>${comidas[i]}</li>`);
-}
-document.write(`</ul>`);
+mostrarComidas();
 
 // agregar un elemento en una posicion particular del array
 comidas.splice(6, 0, 'fideos')
-
-document.write(`<h3>Lista de comidas, elementos: ${comidas.length}</h3>`);
-document.write(`<ul>`);
-for(let i = 0; i < comidas.length; i++){
-    document.write(`<li>${comidas[i]}</li>`);
-}
-document.write(`</ul>`);
+mostrarComidas();
 
 //borrar elementos de un array
 //borrar el primer elemento
 comidas.shift();
-
-document.write(`<h3>Lista de comidas, elementos: ${comidas.length}</h3>`);
-document.write(`<ul>`);
-for(let i = 0; i < comidas.length; i++){
-    document.write(`<li>${comidas[i]}</li>`);
-}
-document.write(`</ul>`);
+mostrarComidas();
 
 //borrar el ultimo elemento
 comidas.pop();
-
-document.write(`<h3>Lista de comidas, elementos: ${comidas.length}</h3>`);
-document.write(`<ul>`);
-for(let i = 0; i < comidas.length; i++){
-    document.write(`<li>${comidas[i]}</li>`);
-}
-document.write(`</ul>`);
+mostrarComidas();
 
 //borrar uno o varios elementos de una posicion particular
 // comidas.splice(7,1);
 // comidas.splice(7,2);
 comidas.splice(7);
-
-document.write(`<h3>Lista de comidas, elementos: ${comidas.length}</h3>`);
-document.write(`<ul>`);
-for(let i = 0; i < comidas.length; i++){
-    document.write(`<li>${comidas[i]}</li>`);
-}
-document.write(`</ul>`);
+mostrarComidas();
 
 document.write(`<p>Comida de la posicion 10: ${comidas[10]} </p>`);
 
 // modifico un valor
 // comidas.splice(4,1,'Lasagna');
 comidas[4] = 'Lasagna';
-
-document.write(`<h3>Lista de comidas, elementos: ${comidas.length}</h3>`);
-document.write(`<ul>`);
-for(let i = 0; i < comidas.length; i++){
-    document.write(`<li>${comidas[i]}</li>`);
-}
-document.write(`</ul>`);
+mostrarComidas();
