83256a0f2f4d078baf5bf1d82cf797ca71e1d5cd
Emilse Arias <emilsearias@gmail.com>
2024-07-15 10:21:33 -0300

    agregamos un ejemplo de funcion con y sin parametros

--- js/06-funciones.js
+++ js/06-funciones.js
@@ -1,14 +1,27 @@
 //funciones declarativas
 //funcion sin parametros
-
 function saludar(){
     //todo el codigo que quiero agregar para saludar
     document.write('<p>Hola a mundo 🖐</p>')
 }
 
+function saludarPersona(nombre, apellido){
+    console.log(nombre);
+    console.log(apellido);
+    const nombreCompleto = nombre +', '+apellido;
+    document.write(`<p>Hola ${nombreCompleto} 🖐</p>`)
+}
+
 //invocar o llamar a una funcion
 saludar();
-
 document.write('<p>tareas varias</p>');
-
 saludar();
+
+const nombre = prompt('Ingresa tu nombre');
+const apellido = prompt('Ingresa tu apellido');
+
+saludarPersona(nombre,apellido);
+saludarPersona('Felicitas','Ralle');
+saludarPersona('Elian', 20);
+
+document.write(`<p>Otra prueba de nombre ${nombreCompleto} </p>`)
