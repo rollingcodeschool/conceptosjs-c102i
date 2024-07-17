c437aa26dc4765edcade00ddbafcac27c9b81a52
Emilse Arias <emilsearias@gmail.com>
2024-07-17 10:38:10 -0300

    agregamos un ejemplo de operador ternario, map y foreach

--- js/05-arrays.js
+++ js/05-arrays.js
@@ -14,15 +14,23 @@ const comidas = [
 ];
 
 //aqui agrego las funciones
-const mostrarComidas = ()=>{
-    document.write(`<h3>Lista de comidas, elementos: ${comidas.length}</h3>`);
-    document.write(`<ul>`);
-    for(let i = 0; i < comidas.length; i++){
-        document.write(`<li>${comidas[i]}</li>`);
-    }
-    document.write(`</ul>`);
-}
+const mostrarComidas = () => {
+  document.write(`<h3>Lista de comidas, elementos: ${comidas.length}</h3>`);
+  document.write(`<ul>`);
+  for (let i = 0; i < comidas.length; i++) {
+    document.write(`<li>${comidas[i]}</li>`);
+  }
+  document.write(`</ul>`);
+};
 
+const mostrarArray = (arreglo, titulo) => {
+  document.write(`<h3>${titulo}, elementos: ${arreglo.length}</h3>`);
+  document.write(`<ol>`);
+  arreglo.map((item, posicion) =>
+    document.write(`<li>${item} - posicion del array:${posicion}</li>`)
+  );
+  document.write(`</ol>`);
+};
 
 document.write(`<p>El array de comidas tiene ${comidas.length} elementos</p>`);
 document.write(`<p>La primer comida es: ${comidas[0]} </p>`);
@@ -34,20 +42,20 @@ console.log(comidas);
 mostrarComidas();
 
 // modificar elementos en un array
-comidas[2] = 'locro';
+comidas[2] = "locro";
 mostrarComidas();
 
 // agregar elementos en un array
 // al principio
-comidas.unshift('tira de asado','guiso')
+comidas.unshift("tira de asado", "guiso");
 mostrarComidas();
 
 //agregar elementos al final
-comidas.push('ravioles', 'sorrentinos')
+comidas.push("ravioles", "sorrentinos");
 mostrarComidas();
 
 // agregar un elemento en una posicion particular del array
-comidas.splice(6, 0, 'fideos')
+comidas.splice(6, 0, "fideos");
 mostrarComidas();
 
 //borrar elementos de un array
@@ -69,26 +77,51 @@ document.write(`<p>Comida de la posicion 10: ${comidas[10]} </p>`);
 
 // modifico un valor
 // comidas.splice(4,1,'Lasagna');
-comidas[4] = 'Lasagna';
+comidas[4] = "Lasagna";
 mostrarComidas();
 
-//buscar si existe el item pizza 
+//buscar si existe el item pizza
 
-const existePizza = comidas.find((comida)=> comida === 'pizza');
-const existeGuiso = comidas.find((comida)=> comida.includes('guiso'));
-const existeTamales= comidas.find((comida)=> comida === 'tamales');
+const existePizza = comidas.find((comida) => comida === "pizza");
+const existeGuiso = comidas.find((comida) => comida.includes("guiso"));
+const existeTamales = comidas.find((comida) => comida === "tamales");
 
-console.log(existePizza)
-console.log(existeGuiso)
-console.log(existeTamales)
+console.log(existePizza);
+console.log(existeGuiso);
+console.log(existeTamales);
+// if(existePizza){
+//   document.write(`<p>Existe el elemento Pizza: Existe</p>`)
+// }else{
+//   document.write(`<p>Existe el elemento Pizza: no existe</p>`)
+// }
 
-const prueba= 'hola mundo'
-console.log(prueba.includes('hola'))
-console.log(prueba.includes('javascript'))
+// operador ternario ? :
+// (condicion logica)? el codigo cuando se cumple : el codigo cuando no se cumple
+(existePizza)  ? document.write(`<p>Existe el elemento Pizza: Existe</p>`)  : document.write(`<p>Existe el elemento Pizza: no existe</p>`);
+existeTamales
+  ? document.write(`<p>Existe el elemento Tamales: Existe</p>`)
+  : document.write(`<p>Existe el elemento Tamales: no existe</p>`);
 
-comidas.push('pizza común', 'pizza especial');
+// if(existeTamales){
+//   document.write(`<p>Existe el elemento Tamales: Existe</p>`)
+// }else{
+//   document.write(`<p>Existe el elemento Tamales: no existe</p>`)
+// }
+
+const prueba = "hola mundo";
+console.log(prueba.includes("hola"));
+console.log(prueba.includes("javascript"));
+
+comidas.push("pizza común", "pizza especial");
 mostrarComidas();
 
 //mostrar todas las pizzas
-const listaPizzas = comidas.filter((alimento)=> alimento.includes('pizza'));
+const listaPizzas = comidas.filter((alimento) => alimento.includes("pizza"));
 console.log(listaPizzas);
+
+//implementar o usar la funcion que muestra arrays
+mostrarArray(listaPizzas, "Lista de pizzas");
+mostrarArray(comidas, "Lista de comidas");
+
+// foreach
+listaPizzas.forEach((item) => console.log("item foreach - " + item));
